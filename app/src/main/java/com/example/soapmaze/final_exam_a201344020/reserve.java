cca8c68a980d98070d326ca06add7cd73cb811ae
soapmaze <lee141567@naver.com>
2016-12-09 10:09:09 +0900

    프로젝트 생성.

---
+++ app/src/main/java/com/example/soapmaze/final_exam_a201344020/reserve.java
@@ -0,0 +1,13 @@
+package com.example.soapmaze.final_exam_a201344020;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reserve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve);
+    }
+}
